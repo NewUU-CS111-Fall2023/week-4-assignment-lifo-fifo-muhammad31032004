a28a54908562c489189ce4558ae3a6b58cd1aca5
muhammad31032004 <127374110+muhammad31032004@users.noreply.github.com>
2023-11-08 16:32:27 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,24 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+class Node {
+public:
+    int data;
+    Node* next;
+    
+    Node(int val){
+        this->data = val;
+        this->next = NULL;
+    }
+};
+
+class Stack{
+    Node* head;
+public:
+    Stack(){
+        this->head = NULL;
+    }
+    void push(int val){
+        Node* tmp = new Node(val);
+        tmp->data = val;
+        tmp->next = this->head;
+        this->head = tmp;
+    }
+};
